747f8364a3bbbe0682fbd825d914acc027e5d631
khadija ennasraoui <ennasraouikhadija@gmail.com>
2023-09-19 00:18:16 +0100

    4 commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ '3FG Aluminuim' }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
@@ -19,10 +19,13 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ '3FG Aluminuim' }}
-                </a>
+
+                <a class="navbar-brand" href="http://127.0.0.1:8000">3FG ALUMINUIM</a>
+
+               
+
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
